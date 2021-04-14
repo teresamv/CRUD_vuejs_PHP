86c7b5b9d2b22daaf7df68731aa476d61e6663af
Sonia <48427184+teresamv@users.noreply.github.com>
2021-04-14 13:03:31 -0500

    Add files via upload

--- action.php
+++ action.php
@@ -48,16 +48,12 @@ if($received_data->action == 'fetchall')
   {
     $data1[] = $row1;
   }
-  // echo json_encode($data);
   echo json_encode(array("data"=>$data,
                         "data1"=>$data1));
 }
 
 if($received_data->action == 'fetchcategory')
 {
-  // $query = $conn->query("SELECT  A.id as docId,B.id as CategoryId,B.category
-  //                        FROM documents A
-  //                        LEFT JOIN categories B ON B.id=A.category_id");
   $query = $conn->query("SELECT * FROM categories");
    
   while($row = $query->fetch_assoc())
@@ -76,20 +72,24 @@ if($received_data->action == 'insert')
   'name' => $received_data->Document
   );
 
-  $check="SELECT * FROM documents WHERE category_id = '".$received_data->Category."' AND name='".$received_data->Document."'";
-//exit;
+  $check="SELECT * 
+          FROM documents 
+          WHERE category_id = '".$received_data->Category."' 
+          AND name='".$received_data->Document."'";
+
   $rs = mysqli_query($conn,$check);
-  $res = mysqli_fetch_array($rs, MYSQLI_NUM);
-  //print_r($res);exit;
-  if($res->num_rows > 0) {
-      $output = array(
+  
+  if (mysqli_num_rows($rs) != 0)
+  {
+    $output = array(
       'message' => "Document Already in Exists<br/>"
     );
   }
 
   else
   {
-    $query = $conn->query("INSERT INTO documents (category_id,name) VALUES ($received_data->Category,'$received_data->Document')");
+    $query = $conn->query("INSERT INTO documents (category_id,name) 
+                           VALUES ($received_data->Category,'$received_data->Document')");
 
 
     $output = array(
@@ -102,50 +102,61 @@ if($received_data->action == 'insert')
 
 if($received_data->action == 'fetchSingle')
 {
- $query = $conn->query("SELECT * FROM documents WHERE id = '".$received_data->id."'");
+  $query = $conn->query("SELECT * FROM documents WHERE id = '".$received_data->id."'");
  
- //$result = $mysqli->query($query);
-
- //$result1 = mysqli_fetch_row($result);
-//print_r($result1);
- //foreach($result as $row)
- //{
-
-while ($row = $query->fetch_assoc()) {
-  //var_dump($row);exit;
-  $data['id'] = $row['id'];
-  $data['category_id'] = $row['category_id'];
-  $data['document'] = $row['name'];
-}
-  
- //}
+  while ($row = $query->fetch_assoc()) {
+    $data['id'] = $row['id'];
+    $data['category_id'] = $row['category_id'];
+    $data['document'] = $row['name'];
+  }
 
 
- $query1 = $conn->query("SELECT * FROM categories");
+  $query1 = $conn->query("SELECT * FROM categories");
    
   while($row1 = $query1->fetch_assoc())
   {
     $data1[] = $row1;
   }
-  //echo json_encode($data);
+ 
  echo json_encode(array("data"=>$data,
                         "data1"=>$data1));
 }
+
 if($received_data->action == 'update')
 {
- $data = array(
+  $data = array(
   'category_id' => $received_data->Category,
   'document' => $received_data->Document,
   'id'   => $received_data->hiddenId
- );
+  );
 
- $query = $conn->query("UPDATE documents SET category_id=$received_data->Category, name = '$received_data->Document' WHERE id = $received_data->hiddenId");
+  $check="SELECT * 
+          FROM documents 
+          WHERE category_id = '".$received_data->Category."' 
+          AND name='".$received_data->Document."' 
+          AND id != '".$received_data->hiddenId."'";
 
- 
+  $rs = mysqli_query($conn,$check);
+  
+  if (mysqli_num_rows($rs) != 0)
+  {
+    $output = array(
+      'message' => "Document Already in Exists<br/>"
+    );
+  }
 
- $output = array(
-  'message' => 'Data Updated'
- );
+  else
+  {
+    $query = $conn->query("UPDATE documents 
+                           SET category_id=$received_data->Category, 
+                           name = '$received_data->Document',
+                           updated_at=CURRENT_TIMESTAMP() 
+                           WHERE id = $received_data->hiddenId");
+
+    $output = array(
+      'message' => 'Data Updated'
+    );
+  }
 
  echo json_encode($output);
 }
@@ -154,9 +165,7 @@ if($received_data->action == 'delete')
 {
  $query = $conn->query("DELETE FROM documents WHERE id = '".$received_data->id."'");
 
- // $statement = $connect->prepare($query);
-
- // $statement->execute();
+ 
 
  $output = array(
   'message' => 'Data Deleted'
